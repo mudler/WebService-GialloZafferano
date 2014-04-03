51c774028eb2c7d46d177d4c5cee8f4d88d3c8a3
mudler <mudler@dark-lab.net>
2014-04-03 23:50:15 +0200

    added cpanfile

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+WebService::GialloZafferano - Blah blah blah
+
+# SYNOPSIS
+
+    use WebService::GialloZafferano;
+
+# DESCRIPTION
+
+WebService::GialloZafferano is
+
+# AUTHOR
+
+mudler <mudler@dark-lab.net>
+
+# COPYRIGHT
+
+Copyright 2014- mudler
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/WebService/GialloZafferano.pm
@@ -0,0 +1,39 @@
+package WebService::GialloZafferano;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::GialloZafferano - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use WebService::GialloZafferano;
+
+=head1 DESCRIPTION
+
+WebService::GialloZafferano is
+
+=head1 AUTHOR
+
+mudler E<lt>mudler@dark-lab.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- mudler
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
